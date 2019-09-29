2aa76a98bbf287dad4e98378f15e9c11304b5e50
shouro <sourogts@gmail.com>
2019-09-29 23:20:33 +0600

    very small cleanups

--- server.go
+++ server.go
@@ -19,34 +19,37 @@ func randomString() string {
 	charset := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 	rand.Seed(time.Now().UnixNano())
 	b := make([]byte, 8)
+
 	for i := range b {
 		b[i] = charset[rand.Intn(len(charset))]
 	}
+
 	return string(b)
 }
 
 func makeASCII(imgPath string) string {
 	srcImg, err := imaging.Open(imgPath, imaging.AutoOrientation(true))
+
 	if err != nil {
 		log.Fatalf("failed to open image: %v", err)
 		return ""
 	}
-	width := srcImg.Bounds().Max.X
-	height := srcImg.Bounds().Max.Y
-	//width := 960
-	//height := 0
+
+	width := 960
+	height := 0
 	var dark uint8
 	resizedImg := imaging.Resize(srcImg, width, height, imaging.Lanczos)
 	ascii := asciify.ToASCII(resizedImg, dark)
 	out := ""
+
 	for _, row := range ascii {
 		out = out + row + "\n"
 	}
+
 	return out
 }
 
 func main() {
-	//defer profile.Start().Stop()
 	r := gin.Default()
 	// Set a lower memory limit for multipart forms (default is 32 MiB)
 	r.MaxMultipartMemory = 8 << 20 // 8 MiB
@@ -71,7 +74,6 @@ func main() {
 		session := sessions.Default(c)
 		file, err := c.FormFile("imagefile")
 		if err != nil {
-			//c.String(http.StatusBadRequest, fmt.Sprintf("get form err: %s", err.Error()))
 			session.Set("flash", "Please select a file.")
 			session.Save()
 			c.Redirect(http.StatusMovedPermanently, "/")
@@ -81,7 +83,6 @@ func main() {
 		filename := filepath.Base(file.Filename)
 		imgPath := "/tmp/" + filename + ".image"
 		if err := c.SaveUploadedFile(file, imgPath); err != nil {
-			//c.String(http.StatusBadRequest, fmt.Sprintf("upload file err: %s", err.Error()))
 			log.Printf("failed to save file in server: %s\n", err.Error())
 			session.Set("flash", "Something went wrong!!, failed to save file in server.")
 			session.Save()
@@ -97,5 +98,6 @@ func main() {
 		}
 		c.Redirect(http.StatusMovedPermanently, "/")
 	})
+
 	r.Run()
 }
